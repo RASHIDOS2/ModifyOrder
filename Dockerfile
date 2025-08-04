9396a7aa4f4fa4895dc45caddeb137057ad3edf4
Rashid <mukhamedov909@mail.ru>
2025-08-04 18:05:49 +0300

    Доработочка

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,10 @@ FROM python:3.11.4
 ENV PYTHONUNBUFFERED=1
 
 WORKDIR /app
+
 COPY ./requirements.txt ./
 
-COPY . .
-
-RUN /usr/loacl/bin/python -m pip install --no-cache-dir --no-warn-script-location --upgrade pip \
+RUN /usr/local/bin/python -m pip install --no-cache-dir --no-warn-script-location --upgrade pip \
     && pip install --no-cache-dir --no-warn-script-location -r requirements.txt
 
 COPY . .
-
-CMD ["sh", "-c", "python manage.py migrate.py migrate && gunicorn reverence.wsgi:application --bind 0.0.0.0:8000"]
--- modifyDoors/settings.py
+++ modifyDoors/settings.py
@@ -87,12 +87,21 @@ WSGI_APPLICATION = 'modifyDoors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEV:
+    DATABASES = {
+        'default': env.db('DATABASE_URL', engine=env.str('DB_ENGINE'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env('POSTGRES_DB'),
+            'USER': env('POSTGRES_USER'),
+            'PASSWORD': env('POSTGRES_PASSWORD'),
+            'HOST': env('POSTGRES_HOST'),
+            'PORT': env('POSTGRES_PORT'),
+        }
+    }
 
 
 # Password validation
